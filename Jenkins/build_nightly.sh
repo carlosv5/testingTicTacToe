b05146f7ae344225f400123118cf60fbead72710
Carlos V <c.vegag5@gmail.com>
2019-06-08 16:48:24 +0200

    Almost finish Nightly job

--- Jenkins/build_nightly.sh
+++ Jenkins/build_nightly.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 TIME=$(date +%Y%d%m)    
 VERSION=$(grep version pom.xml | grep SNAP | cut -c11-15)
-docker build -t testingupmforge/tictactoe_$VERSION.nightly.$TIME:latest target/.
+docker build -t testingupmforge/tictactoe:latest ./target/.
+docker tag testingupmforge/tictactoe:latest testingupmforge/tictactoe:$VERSION.nightly.$TIME
+docker login -u="testingupmforge" -p="carl*sys3rgi*123"
+docker push testingupmforge/tictactoe:$VERSION.nightly.$TIME
