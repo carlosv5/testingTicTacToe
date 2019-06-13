63fad46dfcdadce4a07d30d840e4d8db4bea0ea4
Carlos V <c.vegag5@gmail.com>
2019-06-13 19:18:19 +0200

    Fix script

--- Jenkins/build_nightly.sh
+++ Jenkins/build_nightly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TIME=$(date +%Y%d%m)    
-VERSION=$(grep version pom.xml | grep SNAP | cut -c11-15)
+VERSION=$(grep SNAP pom.xml | grep -o -P '(?<=>).*(?=<)' | tr -d "\-SNAPSHOT")
 docker build -t testingupmforge/tictactoe:latest ./target/.
 docker tag testingupmforge/tictactoe:latest testingupmforge/tictactoe:$VERSION.nightly.$TIME
 docker login -u="testingupmforge" -p="carl*sys3rgi*123"
